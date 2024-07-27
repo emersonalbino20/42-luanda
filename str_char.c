00ea31cf49174dce684ff095dd589771cd135750
Emerson Albino <157174813+emersonalbino20@users.noreply.github.com>
2024-07-27 09:32:03 +0100

    Add files via upload
    Update

--- str_char.c
+++ str_char.c
@@ -1,12 +1,35 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-	char teste[] = {'a', 'n', 'g', 'o', 'l', 'a'};
-	int i = 0;
-	while(i < 6)
+	int tamanhoString(char string[]);
+	char stringUsuario[20];
+	write(1,"Informe uma palavra:\n", 21);
+	scanf("%s", stringUsuario);
+	
+	int size = tamanhoString(stringUsuario);
+	printf(" O tamanho de caracteres incluindo o nulo e %i \n", size);
+	
+	return 0;
+}
+int tamanhoString(char string[])
+{
+	int numCaracteres = 0;
+	
+	while(string[numCaracteres] != '\0')
 	{
-		write(1, &teste[i], 1);
-		i++;
+		numCaracteres++;
 	}
+	
+	printf(" Quantidade de letras digitadas e %i \n", numCaracteres);
+	int nulo = ++numCaracteres;
+
+	if(string[nulo] == '\0')
+	{
+		write(1,"igual\n",6);
+		
+	}
+	
+	return nulo;
 }
